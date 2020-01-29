8b7c2a955252843a940522e07bdd36247278be52
nick <njhylands@gmail.com>
2020-01-28 19:09:39 -0500

    Added story class

---
+++ Story.py
@@ -0,0 +1,20 @@
+class Story:
+    __init__(self):
+        self.title = ""
+        self.author = ""
+        self.summary = ""
+        self.chapter_count = 0
+        self.category = ""
+        self.word_count = 0
+        self.review_count = 0
+        self.favorite_count = 0
+        self.follow_count = 0
+        self.update_date = ""
+        self.publish_date = ""
+        self.status = ""
+        self.series = []
+        self.story_id = ""
+        self.story_link = ""
+        self.rating = 0
+        self.comments = ""
+
